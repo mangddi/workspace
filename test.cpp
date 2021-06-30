1265323a654b828bda94b2f2e95eac7f8e2f618e
mangddi <gksgks8854@gmail.com>
2021-06-30 21:11:03 +0900

    first commit

---
+++ test.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 3;
+    printf("%d", a);
+    
+    return 0;
+}
---
+++ test2.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("daad");
+
+    return 0;
+}
